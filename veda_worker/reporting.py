6200f4e3d67bf43358c5f97ba43f59da5cfee795
Greg Martin <greg@edx.org>
2016-09-01 15:51:27 -0400

    added ses properties

--- veda_worker/reporting.py
+++ veda_worker/reporting.py
@@ -1,6 +1,9 @@
 
 import os
 import sys
+import socket
+import yaml
+import boto.ses
 
 """
 Let's do some quick and dirty error handling & logging
@@ -8,6 +11,25 @@ Let's do some quick and dirty error handling & logging
 """
 
 from global_vars import *
+
+class Credentials():
+
+    def __init__(self):
+        self.auth_yaml = os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            'instance_config.yaml'
+            )
+        self.auth_dict = self._AUTH()        
+
+    def _AUTH(self):
+        if not os.path.exists(self.auth_yaml):
+            return None
+        with open(self.auth_yaml, 'r') as stream:
+            try:
+                return yaml.load(stream)
+            except yaml.YAMLError as exc:
+                return None
+
 
 """
 Unspecified errors with a message
@@ -78,14 +100,52 @@ class Output(object):
         sys.stdout.flush()
 
 
+class EmailAlert():
+
+    def __init__(self, **kwargs):
+        self.auth_dict = Credentials().auth_dict
+        self.message = kwargs.get('message', None)
+
+        self.recipients = [
+            'greg@edx.org',
+            ]
+        self.ipaddy = socket.gethostbyname(socket.gethostname())
+        self.sender = 'veda-noreply@edx.org'
+
+
+    def email(self):
+        email_subject = '[ VEDA ALERTING ]'
+        email_subject += ' : Worker Node Fault : '
+        email_subject += self.ipaddy
+
+        email_body = 'There has been a fault in a veda worker:\n'
+        email_body += self.message + '.\n'
+        email_body += self.ipaddy
+        email_body += ' : This worker has been terminated\n'
+
+        conn = boto.ses.connect_to_region(
+            'us-east-1',
+            aws_access_key_id=self.auth_dict['abvid_access_key_id'],
+            aws_secret_access_key=self.auth_dict['abvid_secret_access_key']
+            )
+
+        conn.send_email(
+            self.sender,
+            email_subject,
+            email_body,
+            self.recipients
+            )
+
 
 
 """Just to sneak a peek"""
 def main():
-    test_error = "This is a test"
-    E1 = ErrorObject.print_error(
-        message = test_error, 
+    daemon_error = "Disk Free Space Alert"
+
+    E2 = EmailAlert(
+        message=daemon_error
         )
+    E2.email()
 
 
 
